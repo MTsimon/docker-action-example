ea4c3257fab9fc9e43f1bf090f6c739529cfa4b8
Simon Møller <simon@micro-technic.com>
2023-02-11 11:15:13 +0100

    toolchain added

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,1 @@
 #!/bin/bash
-
-
-echo "Installing required development tools..."
-apt-get update -y
-apt-get upgrade -y
-apt-get install build-essential -y
-apt-get install git -y
-
-echo "Argument 1 =  $1" #varenummer
-echo "Argument 2 =  $2" # rootfs
-# build sqfs file from roots input argument
